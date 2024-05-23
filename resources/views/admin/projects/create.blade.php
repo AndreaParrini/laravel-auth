1cc9576f69180dd4d4860372186eec351f6e12ad
AndreaParrini <andrea.parrini26@gmail.com>
2024-05-23 17:09:27 +0200

    add view creat a new project

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.admin');
+
+@section('content')
+    <div class="container">
+        <h3 class="text-center">Create a new project</h3>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="{{ route('admin.projects.store') }}" method="post">
+            @csrf
+
+            <div class="mb-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
+                    aria-describedby="helpId" placeholder="Es. molestiae cupiditate illo aut" value="{{ old('title') }}" />
+                <small id="helpId" class="form-text text-muted">Inserte here a title of your project</small>
+                @error('title')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="content" class="form-label">Content</label>
+                <textarea class="form-control" name="content" id="content" rows="3">{{ old('content') }}</textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+                Create
+            </button>
+
+        </form>
+    </div>
+@endsection
