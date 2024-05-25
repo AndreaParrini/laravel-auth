ea1dcb192e55a3ed46c461f865f3f96f760a6a92
AndreaParrini <andrea.parrini26@gmail.com>
2024-05-25 15:34:55 +0200

    add methods to relationship one to many on models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'content', 'cover_image', 'slug'];
+
+    /**
+     * Get the type that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
