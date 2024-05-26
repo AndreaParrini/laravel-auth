d9c9d331de6a6fe4f542cdce9af84e4570f310cf
AndreaParrini <andrea.parrini26@gmail.com>
2024-05-26 14:55:31 +0200

    remove rounde in jumbotron in welcom view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="jumbotron p-5 mb-4 bg-light rounded-3">
+    <div class="jumbotron p-5 mb-4 bg-light">
         <div class="container py-5">
             <div class="logo_laravel">
                 <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
